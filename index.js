63c0d98d165986c0427be4a414462379e8ea912f
StormCodi <121410908+StormCodi@users.noreply.github.com>
2023-03-12 15:30:15 -0700

    created about page and changed up some things

--- index.js
+++
@@ -1,7 +0,0 @@
-function goToUrl() {
-    if (location.href === 'https://stormcodi.github.io/CritiKicks/' || location.href === 'http://127.0.0.1:5500/index.html') {
-      location.reload();
-    } else {
-      location.href = 'https://stormcodi.github.io/CritiKicks/';
-    }
-  }
