7100756ba7171d012d2d57f76d6873bc4cea28a5
StormCodi <bunderpawl@gmail.com>
2023-03-13 16:02:35 -0700

    test yt icon

--- test.js
+++ test.js
@@ -31,3 +31,19 @@ function create_post() {
     console.log(`\nHere's your HTML:\n${html_template}`);
   }
   
+
+
+
+  function goToUrl() {
+    if (location.href === 'https://stormcodi.github.io/CritiKicks/' || location.href === 'http://127.0.0.1:5500/index.html') {
+      location.reload();
+    } else if (location.href === 'http://127.0.0.1:5500/test.html') {
+      location.href = 'http://127.0.0.1:5500/index.html';
+    } else {
+      location.href = 'https://stormcodi.github.io/CritiKicks/';
+    }
+  }
+  
+  function goToAbout() {
+    window.location.href = "about/about.html";
+  }
