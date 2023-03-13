1bad209c4f3cb10a8ccb804a1867493359e4c493
StormCodi <121410908+StormCodi@users.noreply.github.com>
2023-03-12 20:17:58 -0700

    tried fixing the mobile issue again

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 function goToUrl() {
-    if (location.href === 'https://stormcodi.github.io/CritiKicks/' || location.href === 'http://127.0.0.1:5500/index.html') {
-      location.reload();
-    } else if (location.href === 'http://127.0.0.1:5500/about/about.html') {
-      location.href = 'http://127.0.0.1:5500/index.html';
-    } else {
-      location.href = 'https://stormcodi.github.io/CritiKicks/';
-    }
+  if (location.href === 'https://stormcodi.github.io/CritiKicks/' || location.href === 'http://127.0.0.1:5500/index.html') {
+    location.reload();
+  } else if (location.href === 'http://127.0.0.1:5500/about/about.html') {
+    location.href = 'http://127.0.0.1:5500/index.html';
+  } else {
+    location.href = 'https://stormcodi.github.io/CritiKicks/';
   }
-  
-  function goToAbout() {
-    window.location.href = "about/about.html";
+}
+
+function goToAbout() {
+  window.location.href = "about/about.html";
+}
+
+function checkValue() {
+  if (document.querySelector("#mobile_select").value == "about") {
+    goToAbout()
   }
-  
+
+}
